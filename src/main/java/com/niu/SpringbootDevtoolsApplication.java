16ddc305f64c09032ceae49a7342fcff3c77e416
18300603955 <oceaniu@163.com>
2019-06-24 12:08:02 +0800

    dfsa

--- src/main/java/com/niu/SpringbootDevtoolsApplication.java
+++ src/main/java/com/niu/SpringbootDevtoolsApplication.java
@@ -10,6 +10,10 @@ public class SpringbootDevtoolsApplication {
 		System.out.println("devtools test");
 		System.out.println("devtools test34");
 		System.out.println("devtools test54");
+//		Ctrl+Shift+Alt+/快捷键调出Maintenance页面,选择1.Registry...
+//      勾选compiler.automake.allow.when.app.running复选框
+//		引入Spring-boot-devtools后,只要classpath路径下的文件发生改变
+//		项目就会自动重启,每次改动都会重启,有点太烦人了
 
 		SpringApplication.run(SpringbootDevtoolsApplication.class, args);
 	}
