88fbc54d13dd174d1d335a1ce4dc2b4aad0c7770
18300603955 <oceaniu@163.com>
2019-06-24 11:59:29 +0800

    inti

---
+++ src/main/java/com/niu/SpringbootDevtoolsApplication.java
@@ -0,0 +1,17 @@
+package com.niu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevtoolsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("devtools test");
+		System.out.println("devtools test34");
+		System.out.println("devtools test54");
+
+		SpringApplication.run(SpringbootDevtoolsApplication.class, args);
+	}
+
+}
